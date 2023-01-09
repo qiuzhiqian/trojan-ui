36bd98eab04d7ce14969154faf1e9312a8f0140f
qiuzhiqian <xiamengliang@gmail.com>
2023-01-09 10:56:21 +0800

    完善逻辑

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 
 use eframe::egui;
 
+use std::process::Command;
+use std::path::Path;
+use std::path::PathBuf;
+
 fn main() {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0, 280.0)),
@@ -9,7 +13,7 @@ fn main() {
         ..Default::default()
     };
     eframe::run_native(
-        "Trojan UI Tools",
+        "Trojan Tools",
         options,
         Box::new(|_cc| Box::new(MyApp::default())),
     );
@@ -19,18 +23,19 @@ struct MyApp {
     configs: Vec<String>,
     has_selected: usize,
     started: bool,
+    child_path: String,
+    child_process: Option<std::process::Child>,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        let mut app = Self {
-            configs: Vec::<String>::new(),
+        let app = Self {
+            configs: get_configs(get_current_dir().expect("this is normal path"),"json"),
             has_selected: 0,
             started: false,
+            child_path: find_process("trojan"),
+            child_process: None,
         };
-        app.configs.push("config.json".to_string());
-        app.configs.push("examples/client.json".to_string());
-        app.configs.push("examples/server.json".to_string());
 
         return app;
     }
@@ -82,10 +87,92 @@ impl eframe::App for MyApp {
                     };
                     if ui.button(start_label[current_index]).clicked() {
                         println!("Start to connect {}",self.configs[self.has_selected as usize]);
+                        
+                        if !self.started {
+
+                            if let Ok(mut path) = get_current_dir() {
+                                path.push(&self.configs[self.has_selected as usize]);
+
+                                if path.is_file() {
+                                    self.child_process = Some(Command::new(&self.child_path)
+                                        .args(["-c",path.to_str().expect("this is normal path")])
+                                        .spawn()
+                                        .expect("Failed to start echo process"));
+                                }
+                            }
+                        } else {
+                            if let Some(child) = self.child_process.as_mut() {
+                                child.kill().expect("trojan is not running...");
+                            }
+                        }
+                        
                         self.started = !self.started;
                     }
                 });
             });
         });
     }
+
+    fn on_close_event(&mut self) -> bool {
+        if let Some(x) = self.child_process.as_mut(){
+            x.kill().expect("trojan is not running...");
+        }
+                            
+        return true;
+    }
+}
+
+fn get_configs(path: PathBuf,suffix: &str) -> Vec<String> {
+    let mut result = Vec::new();
+    for entry in path.read_dir().expect("this is not dir") {
+        let entry = entry.expect("this is entry...");
+        if let Some(x) = entry.path().extension() {
+            if x == suffix {
+                result.push(entry.file_name().to_str().expect("is normal string").to_string());
+            }
+        }
+    }
+
+    return result;
+}
+
+fn get_current_dir() -> Result<PathBuf,String> {
+    if let Ok(mut path) = std::env::current_exe() {
+        path.pop();
+        
+        if path.is_dir() {
+            return Ok(path);
+        }
+    }
+    return Err("is not directory path".to_string());
+}
+
+fn find_process(name: &str) -> String {
+    if let Ok(mut path) = get_current_dir() {
+        path.push(name);
+        
+        if path.is_file() {
+            return path.to_str().expect("is not normal path").to_string();
+        }
+    }
+
+    return find_process_from_path(name);
+}
+
+fn find_process_from_path(name: &str) -> String {
+    let key = "PATH";
+    if let Ok(val) = std::env::var(key) {
+        let paths :Vec<&str>= val.split(':').collect();
+
+        for path in paths {
+            let mut filepath = Path::new(path).to_path_buf();
+            filepath.push(name);
+
+            if filepath.is_file() {
+                return filepath.to_str().expect("is not normal path").to_string();
+            }
+        }
+    }
+
+    return "".to_string();
 }
