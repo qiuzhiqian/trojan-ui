3eb470c829758cd86a5ede4345641f44c36269fd
xiamengliang <xiamengliang@uniontech.com>
2023-02-01 17:01:46 +0800

    拆分页面切换功能,添加配置页面

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,8 @@ struct MyApp {
     has_selected: usize,
     started: bool,
     send: Option<tokio::sync::mpsc::Sender<bool>>,
+    input_url: String,
+    page_num: u8,
 }
 
 impl Default for MyApp {
@@ -38,6 +40,8 @@ impl Default for MyApp {
             has_selected: 0,
             started: false,
             send: None,
+            input_url: "".to_string(),
+            page_num: 0,
         };
 
         return app;
@@ -48,64 +52,85 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical(|ui| {
-                ui.heading("Trojan Tools");
-                ui.separator();
-                egui::ScrollArea::vertical()
-                    .max_height(200.0)
-                    //.max_width(150.0)
-                    .auto_shrink([false; 2])
-                    .show(ui, |ui| {
-                   
-                    let item_count = self.configs.configs.len();
-                    for item in 0..item_count {
-                        let layout = egui::Layout::left_to_right(egui::Align::LEFT).with_main_justify(true);
-                        ui.with_layout(layout,|ui|{
-                            if self.started {
-                                ui.set_enabled(false);
-                            } else {
-                                ui.set_enabled(true);
-                            }
-                            
-                            ui.selectable_value(&mut self.has_selected, item, &self.configs.configs[item].remarks);
-                        });
-                    }
-                }).inner;
-
-                ui.separator();
-                ui.horizontal(|ui|{
-                    if self.configs.configs.is_empty() {
-                        ui.set_enabled(false);
-                    }else {
-                        ui.set_enabled(true);
-                    }
-                    if ui.button("Edit").clicked() {
-                        println!("TODO Editor item...");
-                    }
-
-                    let start_label=vec!["Start","Stop"];
-                    let current_index = if !self.started {
-                        0
-                    } else {
-                        1
-                    };
-                    if ui.button(start_label[current_index]).clicked() {
-                        if !self.started {
-                            let config = &self.configs.configs[self.has_selected as usize];
-                            self.send = proxy::start(config);
-                        } else {
-                            if let Some(s) = &self.send {
-                                proxy::stop(s);
-                                self.send = None;
-                            }
-                        }
-                        
-                        self.started = !self.started;
-                    }
-                });
+                match self.page_num {
+                    1 => self.setting_page(ui),
+                    _ => self.main_page(ui),
+                };
+                
             });
         });
     }
 }
+
+impl MyApp {
+    fn main_page(&mut self,ui: &mut egui::Ui) {
+        ui.heading("Trojan Tools");
+        ui.separator();
+        egui::ScrollArea::vertical()
+            .max_height(200.0)
+            //.max_width(150.0)
+            .auto_shrink([false; 2])
+            .show(ui, |ui| {
+            
+            let item_count = self.configs.configs.len();
+            for item in 0..item_count {
+                let layout = egui::Layout::left_to_right(egui::Align::LEFT).with_main_justify(true);
+                ui.with_layout(layout,|ui|{
+                    if self.started {
+                        ui.set_enabled(false);
+                    } else {
+                        ui.set_enabled(true);
+                    }
+                    
+                    ui.selectable_value(&mut self.has_selected, item, &self.configs.configs[item].remarks);
+                });
+            }
+        }).inner;
+
+        ui.separator();
+        ui.horizontal(|ui|{
+            if self.configs.configs.is_empty() {
+                ui.set_enabled(false);
+            }else {
+                ui.set_enabled(true);
+            }
+            if ui.button("Edit").clicked() {
+                println!("TODO Editor item...");
+                self.page_num = 1;
+            }
+
+            let start_label=vec!["Start","Stop"];
+            let current_index = if !self.started {
+                0
+            } else {
+                1
+            };
+            if ui.button(start_label[current_index]).clicked() {
+                if !self.started {
+                    let config = &self.configs.configs[self.has_selected as usize];
+                    self.send = proxy::start(config);
+                } else {
+                    if let Some(s) = &self.send {
+                        proxy::stop(s);
+                        self.send = None;
+                    }
+                }
+                
+                self.started = !self.started;
+            }
+        });
+    }
+
+    fn setting_page(&mut self,ui: &mut egui::Ui) {
+        ui.heading("Add Config");
+        ui.separator();
+        ui.add(egui::TextEdit::singleline(&mut self.input_url).hint_text("trojan://password@domain:port#remarks"));
+        if ui.button("Back").clicked() {
+            self.page_num = 0;
+        }
+    }
+}
+
 
 // /current_dir/config.json
 // $XDG_CONFIG_HOME/trojan_ui/config.json
