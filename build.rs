cbe2df4458a3e95b79c2abf3c6b47a8032a45ab6
xml <xiamengliang@gmail.com>
2024-01-15 14:49:33 +0800

    优化主题颜色

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    slint_build::compile_with_config(
+        "ui/appwindow.slint",
+        slint_build::CompilerConfiguration::new()
+            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+    ).unwrap();
 }
